a3c517a1e270e00fcddd126253ac20d144991b45
Ho Nyeong <dev.honing@gmail.com>
2023-12-04 02:14:44 +0900

    서버 제작과 라우팅 처리

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 // app.js
 
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// 미들웨어 설정
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// 기본 라우트 설정
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`서버 ON: http://localhost:${port}`);
+});
