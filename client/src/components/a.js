c5af2378dfeeeab8a73d5b267072c49e18e30112
Ho Nyeong <dev.honing@gmail.com>
2023-12-04 04:06:53 +0900

    컴포넌트 적용이 안되는데 이유를 모르겠다.

---
+++ client/src/components/a.js
@@ -0,0 +1,13 @@
+// a.js
+
+import React from 'react';
+
+const AComponent = () => {
+  return (
+    <div>
+      <h1>대체 왜 안되는데?</h1>
+    </div>
+  );
+};
+
+export default AComponent;
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Test from './components/test/test.js';  // Test 컴포넌트 import
 import NewComponent from './components/NewComponent.js';  // NewComponent 컴포넌트 import
+import AComponent from './components/a.js';
+
 
 ReactDOM.render(
   <NewComponent />,
