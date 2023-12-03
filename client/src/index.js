2b05e0e2c5848897c1e6ecf18a302d6900c5988e
Ho Nyeong <dev.honing@gmail.com>
2023-12-04 02:39:46 +0900

    설정을 완료했지만 test.js의 Test 컴포넌트를 렌더링하지 못하고 있다.

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,10 @@
 // client/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Test from './components/test/test.js';  // Test 컴포넌트 import
+
+ReactDOM.render(
+  <Test />,
+  document.getElementById('root')
+);
