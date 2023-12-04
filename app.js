a512bce9f96aaac95cf90c040718a944045dde3a
Ho Nyeong <dev.honing@gmail.com>
2023-12-04 11:11:41 +0900

    리액트와 웹팩을 사용하면 서버를 제작하고, 라우팅 처리를 할 필요가 없었다.
    - app.js와 route.js도 필요가 없다.
    - 심지어 express 패키지를 설치할 필요도 없었다.

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-// app.js
+// // app.js
 
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
 
-// 미들웨어 설정
-app.use(express.static(path.join(__dirname, 'dist')));
+// // 미들웨어 설정
+// app.use(express.static(path.join(__dirname, 'dist')));
 
-// 기본 라우트 설정
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
-});
+// // 기본 라우트 설정
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+// });
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`서버 ON: http://localhost:${port}`);
-});
+// const port = process.env.PORT || 3001;
+// app.listen(port, () => {
+//   console.log(`서버 ON: http://localhost:${port}`);
+// });
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,12 +1,12 @@
-// server/routes/route.js
+// // server/routes/route.js
 
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-// '/' 경로의 GET 요청에 대한 핸들러
-router.get('/', (req, res) => {
-  // JSON 형식의 응답을 반환해 라우팅 설정이 완료됨을 알림
-  res.json({ message: '라우팅 설정 완료' });
-});
+// // '/' 경로의 GET 요청에 대한 핸들러
+// router.get('/', (req, res) => {
+//   // JSON 형식의 응답을 반환해 라우팅 설정이 완료됨을 알림
+//   res.json({ message: '라우팅 설정 완료' });
+// });
 
-module.exports = router;
+// module.exports = router;
